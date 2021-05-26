cac0bdb034b97f65f3fe11b5d96592db38869e9b
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-05-27 00:03:50 +0530

    Update Assignment no.2.py

--- Assignment no.2.py
+++ Assignment no.2.py
@@ -1,3 +1,13 @@
-side = int(input("enter the side of square :"))
+Write a python code to calculate area of shape.
+
+/***************************************************** SOLUTION **************************************************************************/
+
+side = int(input("Enter the side of square :"))
 area =  (side) *  (side) 
 print(area)
+
+
+/***************************************************** Output **************************************************************************/
+
+Enter the side of square :4
+16
