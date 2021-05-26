cb550f7bba36aa6452013effb3346142782cfadf
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-05-26 23:39:05 +0530

    Add files via upload

---
+++ Assignment no.1.py
@@ -0,0 +1,14 @@
+a = int(input("enter value of a :"))
+b = int(input("enter value of b :"))
+#addition
+c = a + b
+print (c)
+#subtraction
+c = a - b
+print(c)
+#multiplication
+c = a * b
+print (c)
+#division
+c = a / b
+print (c) 
