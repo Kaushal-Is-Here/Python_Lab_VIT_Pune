e46c5f42b0eedfcd12afa54e1dfb523918e41525
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-05-26 23:47:19 +0530

    Update Assignment no.1.py

--- Assignment no.1.py
+++ Assignment no.1.py
@@ -1,14 +1,29 @@
-a = int(input("enter value of a :"))
-b = int(input("enter value of b :"))
+ # Write a program in python to perform Arithmetic Operations (add , subtract , multiplication, Division ) .
+
+
+# /***************************************************** SOLUTION **************************************************************************/
+
+a = int(input("Enter value of a :"))
+b = int(input("Enter value of b :"))
 #addition
 c = a + b
 print (c)
 #subtraction
-c = a - b
-print(c)
+c = a - bS
+print(c)S
 #multiplication
 c = a * b
 print (c)
 #division
 c = a / b
-print (c) 
+print (c)S
+
+
+#***************************************************** Output **************************************************************************/
+
+# Enter value of a :2
+# Enter value of b :4
+# 6 
+# -2
+# 8 
+# 0.5
