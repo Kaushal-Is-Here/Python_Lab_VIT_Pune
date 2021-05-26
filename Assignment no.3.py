52789bc56eb7d0229b72ebde0109526d2d66f515
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-05-27 00:06:42 +0530

    Update Assignment no.3.py

--- Assignment no.3.py
+++ Assignment no.3.py
@@ -1,3 +1,8 @@
+# Write a program in python to perform Arithmetic Operations (add , subtract , multiplication, Division ) .
+
+
+ /***************************************************** SOLUTION **************************************************************************/
+
 roll  =int(input("enter roll of student :")) 
 print(roll)
 name =input("enter name of student :")
@@ -10,4 +15,19 @@ maths = int(input("enter marks of maths :"))
 print(maths)
 avg = float((physics+chemistry+maths)/3) 
 print(" average of 3 subject :",avg)
+
+/***************************************************** Output **************************************************************************/
+
+
+enter roll of student :16
+16
+enter name of student :A
+A
+enter marks of physics :100
+100
+enter marks of chemistry :90
+90
+enter marks of maths :100
+100
+ average of 3 subject : 96.66666666666667
 
