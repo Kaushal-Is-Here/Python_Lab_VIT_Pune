99f1e7cd842255676c3fb67edb7fbbef89496052
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-05-27 00:16:07 +0530

    Update Assignment no.5.py

--- Assignment no.5.py
+++ Assignment no.5.py
@@ -1,5 +1,17 @@
+#A school decided to replace the desks in three classrooms. Each desk sits two students. Given the number of students in each class, print the smallest possible number of desks that can be purchased.
+#The program should read three integers: the number of students in each of the three classes, a, b and c respectively.
+
+/***********************************************Solution*******************************************************/
+
 a = int(input("the number of student in div A :"))
 b = int(input("the number of student in div B :"))
 c = int(input("the number of student in div C :"))
 s = a //2 + b//2 + c//2 + a % 2 + b % 2 + c % 2
 print("total number of desk are required :" , s)
+
+/***********************************************Output*******************************************************/
+
+the number of student in div A :7
+the number of student in div B :8
+the number of student in div C :5
+total number of desk are required : 11
