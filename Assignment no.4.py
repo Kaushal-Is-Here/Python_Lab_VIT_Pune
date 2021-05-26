93d1c259d8623071b9e85f86c8c619d68414c476
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-05-27 00:24:17 +0530

    Update Assignment no.4.py

--- Assignment no.4.py
+++ Assignment no.4.py
@@ -1,2 +1,12 @@
+# write a program that converts Celsius temperatures to Fahrenheit temperatures.
+
+/********************************************Solution**************************************************
+
+celsius = float(input("Enter your celsius value: "))
 fahrenheit = (celsius * 1.8) + 32  
-print((celsius,fahrenheit))  
+print('%0.1f  Celsius is equal to %0.1f degree Fahrenheit'%(celsius,fahrenheit))  
+
+/********************************************Output**************************************************
+
+Enter your celsius value: 100
+100.0  Celsius is equal to 212.0 degree Fahrenheit
