c08a4003f74f4150ec2138ee0f6af5f6a4bbced5
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-06-21 11:06:21 +0530

    Update Lab7.py

--- Lab7.py
+++ Lab7.py
@@ -1,5 +1,6 @@
 #  Write a program that accepts a sentence and calculate the number of letters and digits. Suppose the following input is supplied to the program: hello world! 123 Then, the output should be: LETTERS 10 DIGITS 3
 
+# /***************************************************** SOLUTION **************************************************************************/
 
 s = input("Input a string")
 d=l=0
@@ -12,3 +13,9 @@ for c in s:
         pass
 print("Letters", l)
 print("Digits", d)
+
+#***************************************************** Output **************************************************************************/
+
+#Input a string hello world! 123
+#Letters 10
+#Digits 3
