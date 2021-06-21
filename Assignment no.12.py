6dfd91f9c96077c00c2bbdb9eed271a8fc9cd3a3
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-06-21 11:15:56 +0530

    Update and rename Lab7.py to Assignment no.12.py

---
+++ Assignment no.12.py
@@ -0,0 +1,24 @@
+#  Write a Program to print the given pattern using python.
+
+# /***************************************************** SOLUTION **************************************************************************/
+
+
+rows = int(input("Enter the number of rows"))  
+
+for i in range(0, rows):  
+        for j in range(0, i + 1):  
+            
+            print("*", end="")       
+  
+       
+        print() 
+        
+        
+#***************************************************** Output **************************************************************************/
+
+#Enter the number of rows 5
+#*
+#**
+#***
+#****
+#*****
