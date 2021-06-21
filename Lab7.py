82adac786dec067d66d9e6ff2bee166e727486ac
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-06-21 11:14:26 +0530

    Add files via upload

---
+++ Lab7.py
@@ -0,0 +1,11 @@
+#  Write a Program to print the given pattern using python.
+
+rows = int(input("Enter the number of rows"))  
+
+for i in range(0, rows):  
+        for j in range(0, i + 1):  
+            
+            print("*", end="")       
+  
+       
+        print()  
