e366eaf027c341bf7a9fb4c0a2d43bfc72538dcd
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-06-21 10:46:51 +0530

    Update and rename Lab7.py to Assignment no.8.py

---
+++ Assignment no.8.py
@@ -0,0 +1,17 @@
+#  Write a program to check whether given number is positive or negative
+
+# /***************************************************** SOLUTION **************************************************************************/
+
+
+num = float(input("Enter a number: "))
+if num > 0:
+   print("Positive number")
+elif num == 0:
+   print("Zero")
+else:
+   print("Negative number")
+   
+   #***************************************************** Output **************************************************************************/
+
+#Enter a number: -4
+#Negative number
