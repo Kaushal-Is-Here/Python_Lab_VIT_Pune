d7517e16568a8c14a51359b70e6d6f1ba7fc12bf
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-06-21 10:40:42 +0530

    Update and rename Lab7.py to Assignment no.7.py

---
+++ Assignment no.7.py
@@ -0,0 +1,15 @@
+# Assignment 7. Write a python program to check whether the given number is even/odd.
+
+# /***************************************************** SOLUTION **************************************************************************/
+
+
+a=int(input("Please Enter a Number : "));  
+if(a%2==0):  
+    print("This Number is Even")  
+else:  
+    print("This Number is Odd") 
+    
+#***************************************************** Output **************************************************************************/    
+
+#Please Enter a Number : 2
+#This Number is Even
