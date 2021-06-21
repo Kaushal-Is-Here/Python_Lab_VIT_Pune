b36b5cb94b23994390236fe68d07be2f053352cd
Kaushal-Is-Here <82810925+Kaushal-Is-Here@users.noreply.github.com>
2021-06-21 11:59:21 +0530

    Update Assignment no.15.py

--- Assignment no.15.py
+++ Assignment no.15.py
@@ -1,5 +1,7 @@
 # """ By using list comprehension, write a program to print the list after removing numbers which are divisible by 5 and 7 in [12,24,35,70,88,120,155]"""
 
+# /***************************************************** SOLUTION **************************************************************************/
+
 
 
 row_num = int(input("Input number of rows: "))
@@ -11,3 +13,9 @@ for row in range(row_num):
         multi_list[row][col]= row*col
 
 print(multi_list)
+
+#***************************************************** Output **************************************************************************/
+
+#Input number of rows: 4
+#Input number of columns: 5
+#[[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8], [0, 3, 6, 9, 12]]
